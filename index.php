59dcddeab8c78ae5da4dd96b5398e2bb50866113
Thais Hdz <thaishdzlpz@gmail.com>
2021-02-08 12:55:41 +0000

    Working for all nodes

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
 <?php
+
 function imgNodeRemove($xmlFile)
 {
 	$xmlDoc = new SimpleXMLElement($xmlFile, 0, true);
+	$nodesToDelete = array();
 
+	/**
+	 * First, I do a search for those nodes
+	 * that contain an image node.
+	 */
 	foreach ($xmlDoc as $profile){
+
 		if (isset($profile->img)) {
-			$xmlDOM = dom_import_simplexml($profile);
-			$xmlDOM->parentNode->removeChild($xmlDOM);
+			$nodesToDelete[] = $profile; // add the parent node that contains image node
 		}
-
 	}
-
+	/**
+	 * Then, I remove from the array those nodes
+	 * that contain an image node
+	 */
+	foreach ($nodesToDelete as $node) {
+		/**
+		 * I need to do this because the SimpleXMLElement class has a method
+		 * called upset() but for any reason doesn't work, then I import to
+		 * a DOMElement that contains the removeChild() method
+		 */
+		$xmlDOM = dom_import_simplexml($node);
+		$xmlDOM->parentNode->removeChild($xmlDOM);
+	}
+	// overwriting the file without the image nodes
 	return $xmlDoc->saveXML($xmlFile);
 
 }
